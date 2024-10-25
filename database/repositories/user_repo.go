108655230b31ad4d55cd7b3941bdf760a2a32c5b
Wendler Tenorio <wendler_mendes@hotmail.com>
2024-10-25 01:05:21 -0300

    feat: user repo

---
+++ database/repositories/user_repo.go
@@ -0,0 +1,36 @@
+package repositories
+
+import "github.com/in-memory-rest/database"
+
+type UserRepo interface {
+	FindAll() []database.UserSchema
+	FindById(id database.ID) (database.UserSchema, error)
+	Insert(firstName, lastName, bio string) (database.UserSchema, error)
+	Update(id database.ID, user database.UserSchema) (database.UserSchema, error)
+	Delete(id database.ID) (database.UserSchema, error)
+}
+
+type UserInMemoryRepo struct {
+	DB database.UserDB
+}
+
+func NewUserInMemoryRepo(db database.UserDB) *UserInMemoryRepo {
+	return &UserInMemoryRepo{
+		DB: db,
+	}
+}
+
+func (ur *UserInMemoryRepo) FindAll() []database.UserSchema {
+	return listMapValues(ur.DB)
+
+}
+
+func listMapValues[K comparable, V any](hashMap map[K]V) []V {
+	var values []V
+
+	for _, v := range hashMap {
+		values = append(values, v)
+	}
+
+	return values
+}
---
+++ database/repositories/user_repo_test.go
@@ -0,0 +1,41 @@
+package repositories
+
+import (
+	"github.com/google/uuid"
+	"github.com/in-memory-rest/database"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func setupUserRepo() *UserInMemoryRepo {
+	db := database.NewDB()
+	repo := NewRepo(db)
+
+	return repo.UserRepo
+}
+
+func TestFindAll(t *testing.T) {
+	t.Run("return all registers", func(t *testing.T) {
+		userRepo := setupUserRepo()
+		id := uuid.New()
+		insertedUser := database.UserSchema{
+			Id:        id,
+			FirstName: "John",
+			LastName:  "Doe",
+			Biography: "Nice guy",
+		}
+
+		userRepo.DB[id] = insertedUser
+
+		users := userRepo.FindAll()
+
+		assert.Equal(t, id, users[0].Id)
+	})
+
+	t.Run("when there is no register", func(t *testing.T) {
+		userRepo := setupUserRepo()
+		users := userRepo.FindAll()
+
+		assert.Empty(t, users)
+	})
+}
