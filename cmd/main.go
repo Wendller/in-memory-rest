5d8cec42eb693ddbe3b7abb3d9586b05c312a42d
Wendler Tenorio <wendler_mendes@hotmail.com>
2024-10-26 19:56:04 -0300

    refactor: remove unused function

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"github.com/fatih/structs"
-	"github.com/google/uuid"
 	"github.com/in-memory-rest/configs"
-	"github.com/in-memory-rest/internal/database"
-	"github.com/in-memory-rest/internal/database/repositories"
-	"github.com/in-memory-rest/pkg"
 	"log/slog"
 	"net/http"
 )
@@ -21,28 +15,4 @@ func main() {
 	}
 
 	slog.Info("all systems initialized")
-}
-
-func run() error {
-	db := database.NewDB()
-	repo := repositories.NewRepo(db)
-
-	repo.UserRepo.DB[uuid.New()] = database.UserSchema{
-		Id:        uuid.New(),
-		FirstName: "Wend",
-		LastName:  "Ten",
-		Biography: "Best",
-	}
-
-	users := repo.UserRepo.FindAll()
-
-	user := users[0]
-	userMap := structs.Map(user)
-	changeset := pkg.NewChangesetValidator()
-
-	fields := []string{"FirstName", "LastName", "Bio"}
-
-	fmt.Println(changeset.ValidateRequired(userMap, fields))
-
-	return nil
 }
